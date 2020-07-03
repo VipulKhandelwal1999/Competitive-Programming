a245d82f5b1bb28e69488359e475eaed139cb420
Vipul Khandelwal <44499187+VipulKhandelwal1999@users.noreply.github.com>
2020-07-03 22:21:02 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,18 +1,7 @@
 #include<bits/stdc++.h>
 #define int     long long int
-#define MOD     1000000007
 #define fastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define PI      3.1415926535897932384626
-#define pb      push_back
-#define clr(x)  memset(x, 0, sizeof(x))
-const int N=1000001;
 using namespace std;
-int gcd(int a,int b){if(a==0)return b;return gcd(b%a,a);}
-int lcm(int a,int b){return (max(a,b)/gcd(a,b))*min(a,b);}
-int powermodm(int x,int n,int M){int result=1;while(n>0){if(n % 2 ==1)result=(result * x)%M;x=(x*x)%M;n=n/2;}return result;}
-int gcdExtended(int a, int b, int *x, int *y){if (a == 0){*x = 0, *y = 1;return b;}int x1, y1;int gcd = gcdExtended(b % a, a, &x1, &y1);*x = y1 - (b / a) * x1;*y = x1;return gcd;}
-int modInverse(int a, int m){int x, y;int g = gcdExtended(a, m, &x, &y);if (g != 1)return -1;else{int res = (x % m + m) % m;return res;}}
-bool isPrime(int a){if(a==1) return false;if(a==2) return true;if(a%2==0) return false;int root=sqrt(a);for(int i=3;i<=(int)root;i+=2){if(a%i==0)return false;}return true;}
 
 main()
 {
